74afbc48238274a023b15d6dded13ed1e555b5f7
Isak <aslundisak@gmail.com>
2015-12-30 02:47:01 +0100

    Added names to all users with prompt......

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -7,7 +7,7 @@ var ChatMessage = React.createClass({
       return (
         <div key={ index }>
           <p>
-            <em>Anonym: </em>
+            <em>{item.user}: </em>
             { item.text }
             <i className="fa fa-minus-circle pull-right" onClick={ _this.props.removeItem.bind(null, item['.key']) }
                 style={{ color: '#DC4E41', marginLeft: '10px', cursor: 'pointer' }}></i>
